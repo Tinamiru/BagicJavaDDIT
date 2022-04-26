3164b86fd2d33181a6ff116f6dcf99baa68ba296
JayAl <sjf95@naver.com>
2022-04-26 19:05:30 +0900

    DDIT BagicJava Chapter 6 Array Exam and Class with Exam

---
+++ src/bagicJavaChapter6_Class/Car2Exam.java
@@ -0,0 +1,22 @@
+package bagicJavaChapter6_Class;
+
+public class Car2Exam {
+	public static void main(String[] args) {
+		Car2 car1 = new Car2();
+		System.out.println(car1.company);
+		System.out.println(car1.model);
+		System.out.println(car1.speed);
+		System.out.println(car1.engineStart);
+
+		Car2 car2 = new Car2();
+
+		car2.speed = 100;
+		car2.engineStart = true;
+
+		System.out.println(car2.company);
+		System.out.println(car2.model);
+		System.out.println(car2.speed);
+		System.out.println(car2.engineStart);
+
+	}
+}
---
+++ src/bagicJavaChapter6_Class/Member2.java
@@ -0,0 +1,12 @@
+package bagicJavaChapter6_Class;
+
+public class Member2 {
+	String name;
+	int age;
+
+	public void member(String n, int a) {
+		name = n;
+		age = a;
+	}
+
+}
