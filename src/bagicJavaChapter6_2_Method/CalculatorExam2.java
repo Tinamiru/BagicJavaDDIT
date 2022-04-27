8d121501d5dd14d5bfa792dc734b7ae49ce2c25e
JayAl <sjf95@naver.com>
2022-04-27 13:42:26 +0900

    DDIT BagicJava Chapter 6 Method and Method Exam

---
+++ src/bagicJavaChapter6_2_Method/CalculatorExam2.java
@@ -0,0 +1,8 @@
+package bagicJavaChapter6_2_Method;
+
+public class CalculatorExam2 {
+	public static void main(String[] args) {
+		Calculator2 myCalc = new Calculator2();
+		myCalc.execute();
+	}
+}
---
+++ src/bagicJavaChapter6_2_Method/Printer.java
@@ -0,0 +1,19 @@
+package bagicJavaChapter6_2_Method;
+
+public class Printer {
+	void println(int n) {
+		System.out.println(n);
+	}
+
+	void println(boolean velue) {
+		System.out.println(velue);
+	}
+
+	void println(double d) {
+		System.out.println(d);
+	}
+
+	void println(String str) {
+		System.out.println(str);
+	}
+}
