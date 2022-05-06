41dd94d34bcc23882089176aadfc02eb321cd665
JayAl <sjf95@naver.com>
2022-05-06 13:40:37 +0900

    DDIT BagicJava Chapter 8 Interface

---
+++ src/bagicjavachapter08/Interface.java
@@ -0,0 +1,10 @@
+package bagicjavachapter08;
+
+public class Interface {
+	// 추상클래스와 추상메소드의 선언(abstract)처럼 기본으로 기입되어있으나 명시는 생략되어진다.
+	
+	// 필드의 선언은 불가능하나 상수의 필드 선언은 가능하다.
+	// 이때, public static final이 자동 기입되지만 생략되어지며
+	// 스태틱 파이널 필드와 같이, 선언과 동시에 초기화가 필요하다.
+	// 
+}
---
+++ src/bagicjavachapter08/interface01/RemoteControlExam.java
@@ -0,0 +1,10 @@
+package bagicjavachapter08.interface01;
+
+public class RemoteControlExam {
+	public static void main(String[] args) {
+
+		RemoteControl rc;
+		rc = new Television();
+		rc = new Audio();
+	}
+}
