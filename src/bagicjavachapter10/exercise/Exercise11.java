d9a57f8d5ab4d0ea1d3cf28deb04290a6ad728e4
JayAl <sjf95@naver.com>
2022-05-12 13:41:04 +0900

    DDIT BagicJava Chapter 10 Exception exercise(Modify).

--- src/bagicjavachapter10/exercise/Exercise11.java
+++ src/bagicjavachapter10/exercise/Exercise11.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 public class Exercise11 {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		while (true) {
 			try {
-				Scanner scanner = new Scanner(System.in);
 				System.out.print("어떤 수를 나누시겠습니까?>>");
 				int input = scanner.nextInt();
 				System.out.print("어떤 수로 나누시겠습니까?>>");
@@ -17,17 +17,17 @@ public class Exercise11 {
 				if (input2 == 0) {
 					throw new RuntimeException("0으로 나눌 수 없습니다. 다시 입력해주세요.");
 				} else {
-					int avg = input / input2;
-					System.out.printf("%d / %d = %d", input, input2, avg);
+					int div = input / input2;
+					System.out.printf("%d / %d = %d", input, input2, div);
 					break;
 				}
 			} catch (InputMismatchException e) {
 				System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
-				continue;
+				scanner.next();
 			} catch (Exception e) {
 				System.out.println(e.getMessage());
 			}
 		}
-
+		scanner.close();
 	}
 }
